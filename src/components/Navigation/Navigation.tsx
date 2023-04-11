a93ea385a0ae11d3df6ad0fb1f06c1edff195d28
levaniakobidze <levaniakobidze25@gmail.com>
2023-04-11 15:44:37 +0400

    feat: clean project. add Navigation. add login page

---
+++ src/components/Navigation/Navigation.tsx
@@ -0,0 +1,65 @@
+import React, { ReactHTMLElement, useState } from "react";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
+
+function Navigation() {
+  const [showMenu, setShowMenu] = useState(false);
+
+  interface InavTypes {
+    name: string;
+    href: string;
+  }
+
+  const navigation: InavTypes[] = [
+    { name: "მთავარი", href: "/" },
+    { name: "მთავარი", href: "/" },
+    { name: "მთავარი", href: "/" },
+    { name: "მთავარი", href: "/" },
+  ];
+
+  const handleClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    e.stopPropagation();
+  };
+
+  return (
+    <div>
+      <nav className="py-5 px-10 flex items-center justify-between z-0">
+        <div className="font-bold text-3xl text-gray-400 cursor-pointer">
+          Logo
+        </div>
+        <div>
+          <MenuIcon
+            onClick={() => setShowMenu(!showMenu)}
+            style={{ fontSize: "44px" }}
+            className={`${
+              showMenu && "text-red-500"
+            } hover:text-red-500 cursor-pointer `}
+          />
+        </div>
+      </nav>
+      <div
+        onClick={() => setShowMenu(false)}
+        className={` absolute top-0 bottom-0  ${
+          !showMenu ? "left-[-100%]" : "left-0   custom-transition "
+        }  
+        transition-all w-full  bg-gradient-to-r 
+            from-[rgba(0,0,0,0.65)] to-[rgba(0,0,0,0.65)] h-[120vh]
+   `}></div>
+      <div
+        className={` ${
+          !showMenu ? "left-[-100%] " : "left-0"
+        } px-10 w-[65%] sm:w-full  absolute top-0 bottom-0 sm:max-w-sm h-[120vh] bg-black transition-all ease-in-out duration-1000`}
+        onClick={handleClick}>
+        <div className="text-right mt-5">
+          <CloseIcon
+            onClick={() => setShowMenu(false)}
+            className="text-white hover:text-red-500 transition cursor-pointer "
+            style={{ fontSize: "44px" }}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navigation;
---
+++ src/pages/auth/login/index.tsx
@@ -0,0 +1,58 @@
+import Navigation from "@/components/Navigation/Navigation";
+import React, { Fragment } from "react";
+
+function Login() {
+  return (
+    <Fragment>
+      <Navigation />
+      <section className="px-10 py-10 w-full h-[100vh] flex flex-col    bg-gradient-to-r to-tl from-[#c7d8e8] to-[#fde6e7]">
+        <div className=" text-center my-5 ">
+          <h1 className="text-black text-4xl font-bold ">ავტორიზაცია</h1>
+          <p className="text-black mt-3 ">შედი შენს ანგარიშზე</p>
+        </div>
+        <div className=" px-5 py-10 w-full sm:mx-auto sm:w-full sm:max-w-lg border rounded-3xl bg-white ">
+          <h1 className="text-center mt-5 font-bold text-xl">
+            შეავსე ველები და შედი შენს ანგარიშზე
+          </h1>
+          <div className="mt-10 ">
+            <label htmlFor="email" className="text-gray-800">
+              ელ.ფოსტა
+            </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="შეიყვანეთ სახელი"
+              className="mt-3 w-full py-2.5 px-3 bg-[#f0f5f9] border-0 outline-none rounded-lg focus:outline focus:outline-cyan-300  text-sm"
+            />
+          </div>
+          <div className="mt-5">
+            <label htmlFor="password" className="text-gray-800">
+              პაროლი
+            </label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="შეიყვანეთ პაროლი"
+              className="mt-3 w-full py-2.5 px-3 bg-[#f0f5f9] border-0 outline-none rounded-lg focus:outline focus:outline-cyan-300  text-sm"
+            />
+          </div>
+          {/* <div className="flex items-center mt-3">
+          <input type="checkbox" name="policy" id="policy" />
+          <label htmlFor="policy" className="mx-3 cursor-pointer">
+            I accept the Terms of Service
+          </label>
+        </div> */}
+          <div>
+            <button className="mt-5 py-3 bg-[#496885] w-full text-white rounded-md hover:bg-[#a1bdd7] ">
+              შესვლა
+            </button>
+          </div>
+        </div>
+      </section>
+    </Fragment>
+  );
+}
+
+export default Login;

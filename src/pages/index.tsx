b2ea851bcf0033bee7b6b04f5b32bc1da28a184d
levaniakobidze <levaniakobidze25@gmail.com>
2023-07-04 00:34:48 +0400

    feat: simple change

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,6 +17,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   const { categories, books } = useContext(BooksContext);
+
   return (
     <Fragment>
       <Navigation />
